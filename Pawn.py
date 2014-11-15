46c7c5d79e4bc7363acba829d6fbd2205b26ea7b
Daniel <daniel.sonner@gmail.com>
2014-11-15 03:27:00 -0800

    used BLACK and WHITE constants

--- Pawn.py
+++ Pawn.py
@@ -5,7 +5,7 @@ class Pawn(MovedPiece):
   def validMove(self,startLoc,endLoc):
     if abs(endLoc[0] - startLoc[0]) > 1:
       return False # moved too far to a side
-    if self.color:
+    if self.color == self.WHITE:
       if start[0] - end[0] == 1:
         return True
       elif start[0] - end[0] == 2 and not self.moved:
