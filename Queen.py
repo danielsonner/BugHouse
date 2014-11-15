783c802f195b1cd18ad0bcdb2c8f8f6e67f92f6a
Daniel <daniel.sonner@gmail.com>
2014-11-15 00:49:56 -0800

    implemented moveLikeRook

--- Queen.py
+++ Queen.py
@@ -6,3 +6,8 @@ class Queen(Piece):
         or abs(((endLoc[1]-startLoc[1])/(endLoc[0] - startLoc[0]))) == 1):
       return True
     return False
+    
+  def moveLikeRook(self, startLoc, endLoc):
+    if startLoc[0] == endLoc[0] or startLoc[1] == endLoc[1]:
+      return True
+    return False
