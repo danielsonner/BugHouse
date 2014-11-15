bcfc5b348540161f376e6f316cfb0de712dbe9d8
Daniel <daniel.sonner@gmail.com>
2014-11-15 00:53:28 -0800

    created SingleGame.py match a board to 2 players

---
+++ SingleGame.py
@@ -0,0 +1,20 @@
+class SingleGame(object):
+  def __init__(self, player1, player2, board, whoseTurn = True):
+    self.player1 = player1
+    self.player2 = player2
+    self.board = board
+    self.whoseTurn = whoseTurn
+    
+  def validMove(self, start, end):
+    """
+    inputs: start is a tuple x coord, y coord and end is tuple with end x, end y
+    outputs: True if move is valid, else False
+    """
+    if self.board.occupantColor(start) != whoseTurn:
+      return False
+    else:
+      # in some cases board throws assertion error if move is invalid
+      try: 
+        return self.board.move(start,end)
+      except AssertionError:
+        return False
