b808f9cc8a037c11378e78487586e5fe25980f05
Daniel <daniel.sonner@gmail.com>
2014-11-15 02:33:16 -0800

    captured pieces are now counted as moved pieces

--- BugHouseGame.py
+++ BugHouseGame.py
@@ -1,3 +1,5 @@
+from MovedPiece import MovedPiece
+
 class BugHouseGame(object):
   """ A Bug House Game is the highest level object we have. 
   It contains 2 single game objects and should be called to
@@ -8,7 +10,10 @@ class BugHouseGame(object):
     self.game2 = singleGame2
     
   def move(self, startLoc, endLoc, game1=True):
-    """Tries to make move. If move is made returns true, else false."""
+    """Attempts to make move. 
+       inputs: startLoc and endLoc are tuples with coordinates x,y
+               A startLoc can be a piece if a piece is being dropped
+       outputs: True if move made, else False"""
     try:
       if game1:
         gamePlaying = self.game1
@@ -18,7 +23,13 @@ class BugHouseGame(object):
         gameNotPlaying = self.game1
       capturedPiece = gamePlaying.makeAndValidateMove(startLoc,endLoc)
       if capturedPiece:
+        # captured pieces are counted as having moved already
+        if isinstance(capturedPiece, MovedPiece):
+          capturedPiece.move()
+          capturedPiece.move()
+        # give the captured piece to the the partner of the capturer
         gameNotPlaying.addPiece(capturedPiece)   
+      # since no errors were thrown, we were able to make a move
       return True
     except AssertionError:
       return False
