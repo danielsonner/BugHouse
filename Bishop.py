fd84ec1c01498816e17c80f806615b49b4a236a9
Daniel <daniel.sonner@gmail.com>
2014-11-15 00:24:13 -0800

    implemented getSlopeOfMove & Pawn move method

--- Bishop.py
+++ Bishop.py
@@ -2,6 +2,8 @@ from Piece import Piece
 
 class Bishop(Piece):
   def validMove(self,startLoc,endLoc):
-    if abs(((endLoc[1]-startLoc[1])/(endLoc[0] - startLoc[0]))) == 1:
+    if abs(self.getSlopeOfMove(startLoc,endLoc)) == 1:
       return True
     return False
+  def getSlopeOfMove(self,startLoc,endLoc):
+    return (endLoc[1]-startLoc[1])/(endLoc[0] - startLoc[0])
